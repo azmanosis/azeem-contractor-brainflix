67b732bfdb95d9a4c593e2b1d5acffb34c9b6071
azmanosis <106712529+azmanosis@users.noreply.github.com>
2022-11-03 00:20:33 -0400

    Header components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.scss';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <>
+    <div>
       {/* Header */}
       <Header />
-    </>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
-import './Header.scss'
+import './Header.scss';
+import logo from '../../assets/logo/BrainFlix-logo.svg';
 
 function Header() {
     return (
-        <>
-            <header>
-
-            </header>
-        </>
+        <div>
+            <img className='' src={logo} alt='logo'></img>
+            <input className="search" placeholder='Search'></input>
+            <div className='user' alt='user'></div>
+        </div>
     )
 }
 
