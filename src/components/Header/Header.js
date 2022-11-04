846b5f46658d53ac5de8b4cf4516527950e21cc8
azmanosis <106712529+azmanosis@users.noreply.github.com>
2022-11-04 01:37:53 -0400

    Header upload button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,13 @@ import logo from '../../assets/logo/BrainFlix-logo.svg';
 function Header() {
     return (
         <div>
-            <img className='' src={logo} alt='logo'></img>
-            <input className="search" placeholder='Search'></input>
-            <div className='user' alt='user'></div>
+            <img className='logo' src={logo} alt='logo'></img>
+            <div className='header'>
+                <input className="header__search" placeholder='Search'></input>
+                <button className='header__upload'>UPLOAD</button>
+                <div className='header__user' alt='user'></div> 
+            </div>
+            <button className='header__upload'>UPLOAD</button>
         </div>
     )
 }
