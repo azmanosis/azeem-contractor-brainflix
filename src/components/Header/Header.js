23312e3c6b93caa2554b1c1874439eff255ca4b8
Azeem Contractor <azeem.contractor@gmail.com>
2022-11-06 14:34:38 -0500

    Header completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,14 @@ import logo from '../../assets/logo/BrainFlix-logo.svg';
 
 function Header() {
     return (
-        <div>
-            <img className='logo' src={logo} alt='logo'></img>
-            <div className='header'>
-                <input className="header__search" placeholder='Search'></input>
-                <button className='header__upload'>UPLOAD</button>
-                <div className='header__user' alt='user'></div> 
+        <div className="header">
+            <div className="header__logo">
+                <img className="header__logo--height" src={logo} alt="brainflix-logo"></img>
+            </div>
+            <div className="header__navigation">
+                <input className="header__navigation--search" placeholder='Search'></input>
+                <button className='header__navigation--upload'>UPLOAD</button>
+                <div className='header__navigation--user' alt='user'></div> 
             </div>
             <button className='header__upload'>UPLOAD</button>
         </div>
