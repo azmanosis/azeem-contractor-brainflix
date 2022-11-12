7e50a8e5e91e53adc9a2af3cb8173da35ac0b36d
Azeem Contractor <azeem.contractor@gmail.com>
2022-11-12 00:25:58 -0500

    Video Styling

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,9 +2,23 @@ import './Video.scss';
 
 function Video (props) {
     return (
-        <div>
-            <video poster={props.poster} className="poster" controls>
+        <div className="container">
+            <video poster={props.poster} className="container__video">
             </video>
+            <div className="container__controls">
+                <div className="container__controls--button">
+                    <img className="container__controls--button--play"></img>
+                </div>
+                <div className="container__controls--timeline">
+                    <div className="container__controls--timeline--bar">
+                        <div className="container__controls--timeline--bar--inner"></div>
+                    </div>
+                </div>
+                <div className="container__controls--fullvolume">
+                    <img className="container__controls--fullvolume--fullscreen"></img>
+                    <img className="container__controls--fullvolume--volume"></img>
+                </div>
+            </div>
         </div>
     )
 }
