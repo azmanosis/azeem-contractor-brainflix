fefcb52de4480d6758e7e6728f1106fee2dbf619
Azeem Contractor <azeem.contractor@gmail.com>
2022-11-13 16:23:43 -0500

    Tablet comepleted

--- src/components/Commentbox/Commentbox.js
+++ src/components/Commentbox/Commentbox.js
@@ -14,8 +14,11 @@ function Commentbox (props) {
                     <form id="submit-comment" className="commentbox__container--b">
                         <label className="commentbox__container--b--words" for="conversation">join the conversation</label>
                         {/* <textarea id="enter__comment" className="commentbox__container--b--input" placeholder="Add a new comment" name="conversation"></textarea> */}
-                        <input id="enter__comment" className="commentbox__container--b--input" type="text" value="" placeholder="Add a new comment" name="conversation"></input>
-                        <button className="commentbox__container--b--button">comment</button>
+                        <div className="commentbox__container--b--inbut">
+                            <input id="enter__comment" className="commentbox__container--b--inbut--input" type="text" value="" placeholder="Add a new comment" name="conversation"></input>
+                            <button className="commentbox__container--b--inbut--button">comment</button>
+                        </div>
+                        
                     </form>
                 </div>
             </div>
