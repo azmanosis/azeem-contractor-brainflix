66f42d2b94992d46806234590722fb4a24f01323
Azeem Contractor <azeem.contractor@gmail.com>
2022-11-15 22:47:40 -0500

    Browser Router – Home Setup

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,44 @@
+import './Home.scss';
+import Header from '../../components/Header/Header';
+import Video from '../../components/Video/Video';
+import { useState } from 'react';
+import Section from '../../components/Section/Section';
+import Commentbox from '../../components/Commentbox/Commentbox';
+import Comment from '../../components/Comments/Comments';
+import Nextvideo from '../../components/Nextvideo/Nextvideo';
+import Data from '../../data/video-details.json';
+
+const Home = () => {
+
+    const [videodetails, setVideoDetails] = useState(Data[0]);
+
+
+    const handleClickVideo = (id) => {
+        Data.map(details => {
+            if (details.id === id) {
+                setVideoDetails(details)
+            }
+        })
+    }
+
+    return (
+        <>
+            {/* Header */}
+            <Header />
+            <Video poster={videodetails.image} />
+            <div className="belowvideo">
+                <div className="belowvideo__sectioncomments">
+                    <Section sections={videodetails} />
+                    <Commentbox commentlength={videodetails.comments.length} commentword={"Comments"} />
+                    <Comment comments={videodetails.comments} />
+                </div>
+                <div className="belowvideo__nextvideo">
+                    <p className="belowvideo__nextvideo--text">next videos</p>
+                    <Nextvideo handleClickVideo={handleClickVideo} nextvideo={Data} activevideoid={videodetails.id} />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
