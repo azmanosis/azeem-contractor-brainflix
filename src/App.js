dca9f1c6368138d252cfdf5b67fe85290c896bee
Azeem Contractor <azeem.contractor@gmail.com>
2022-11-28 23:49:32 -0500

    Axios get request + useEffect and Param trial

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/upload" element={<Uploads />} />
-          <Route path="/:videoId" element={<VideoId />} />;
+          <Route path="/:id" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,90 +1,91 @@
 import './Home.scss';
 import Header from '../../components/Header/Header';
 import Video from '../../components/Video/Video';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Section from '../../components/Section/Section';
 import Commentbox from '../../components/Commentbox/Commentbox';
 import Comment from '../../components/Comments/Comments';
 import Nextvideo from '../../components/Nextvideo/Nextvideo';
-import Data from '../../data/video-details.json';
-
-// Make API call and use params
-import { useParams } from 'react-router-dom';
+// import Data from '../../data/video-details.json';
+import React from 'react'
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-
-// function to use params from api videoId
-function VideoId() {
-    const params = useParams();
-    return <div>{params.videoId}</div>
-}
-
-let apiKey = '';
-
-function getApiKey() {
-    if (apiKey = '') {
-        let userURL = "https://project-2-api.herokuapp.com/register";
-        return axios.get(userURL).then((response) => {
-            apiKey = response.data.api_key;
-            return Promise.resolve(apiKey)
-        })
-    } else {
-        return Promise.resolve(apiKey)
-    }
-}
-
-getVideoData()
-
-axios.get("https://project-2-api.herokuapp.com/register").then((response) => {
-    apiKey = response.data.api_key;
-    return apiKey
-}).then((apiKey) => {
-    axios.get(`https://project-2-api.herokuapp.com/videos?api_key=${apiKey}/`)
-        .then((response) => console.log(response));
-}).catch(error => console.log(error));
-
-function getVideoData() {
-    getApiKey().then((apiKey) => axios.get(`https://project-2-api.herokuapp.com/videos?api_key=${apiKey}/`))
-        .then((response) => {
-            videodetails = response.data
-            Infinity(videodetails)
-        }).catch(error => console.log(error));
-}
-
-let videodetails = []
 
 // Sprint-1 components
 const Home = () => {
+    const { id } = useParams()
+    const url = `https://project-2-api.herokuapp.com/videos?api_key=ce6162de-a18f-4f0a-a1d8-43900ef59215`;
+    const [videos, setVideos] = useState(null);
 
-    const [videodetails, setVideoDetails] = useState(Data[0]);
+    let content = null
 
+    useEffect(() => {
+        axios.get(url)
+            .then(response => {
+                setVideos(response.data)
+            })
+    }, [url])
 
-    const handleClickVideo = (id) => {
-        Data.map(details => {
-            if (details.id === id) {
-                setVideoDetails(details)
-            }
-        })
+    console.log(url)
+
+    if (videos) {
+
+        const handleClickVideo = (id) => {
+            videos.map(details => {
+                if (details.id === id) {
+                    setVideos(details)
+                }
+            })
+        }
+
+        content =
+            <>
+                {/* Header */}
+                <Header />
+                <Video poster={videos.image} />
+                <div className="belowvideo">
+                    <div className="belowvideo__sectioncomments">
+                        <Section sections={videos} />
+                        <Commentbox commentlength={videos.comments.length} commentword={"Comments"} />
+                        <Comment comments={videos.comments} />
+                    </div>
+                    <div className="belowvideo__nextvideo">
+                        <p className="belowvideo__nextvideo--text">next videos</p>
+                        <Nextvideo handleClickVideo={handleClickVideo} nextvideo={videos} activevideoid={videos.id} />
+                    </div>
+                </div>
+            </>
+
     }
 
     return (
-        <>
-            {/* Header */}
-            <Header />
-            <Video poster={videodetails.image} />
-            <div className="belowvideo">
-                <div className="belowvideo__sectioncomments">
-                    <Section sections={videodetails} />
-                    <Commentbox commentlength={videodetails.comments.length} commentword={"Comments"} />
-                    <Comment comments={videodetails.comments} />
-                </div>
-                <div className="belowvideo__nextvideo">
-                    <p className="belowvideo__nextvideo--text">next videos</p>
-                    <Nextvideo handleClickVideo={handleClickVideo} nextvideo={Data} activevideoid={videodetails.id} />
-                </div>
-            </div>
-        </>
+        <div>{content}</div>
     )
+
+
+    // Sprint 1 from Below
+    // const [videodetails, setVideoDetails] = useState(Data[0]);
+
+
+    // return (
+    //     <>
+    //         {/* Header */}
+    //         <Header />
+    //         <Video poster={videodetails.image} />
+    //         <div className="belowvideo">
+    //             <div className="belowvideo__sectioncomments">
+    //                 <Section sections={videodetails} />
+    //                 <Commentbox commentlength={videodetails.comments.length} commentword={"Comments"} />
+    //                 <Comment comments={videodetails.comments} />
+    //             </div>
+    //             <div className="belowvideo__nextvideo">
+    //                 <p className="belowvideo__nextvideo--text">next videos</p>
+    //                 <Nextvideo handleClickVideo={handleClickVideo} nextvideo={Data} activevideoid={videodetails.id} />
+    //             </div>
+    //         </div>
+    //     </>
+    // )
 }
 
 export default Home
