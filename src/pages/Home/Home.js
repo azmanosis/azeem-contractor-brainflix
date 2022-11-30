5ea5c6d273ee2ff3d4b4fd5265b9feba7d2e5c36
Azeem Contractor <azeem.contractor@gmail.com>
2022-11-29 22:56:30 -0500

    commented out code from Fahd

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,52 +16,118 @@ import { useParams } from 'react-router-dom';
 const Home = () => {
     const { id } = useParams()
     const url = `https://project-2-api.herokuapp.com/videos?api_key=ce6162de-a18f-4f0a-a1d8-43900ef59215`;
-    const [videos, setVideos] = useState(null);
+    // const urls = `https://project-2-api.herokuapp.com/videos/${id}?api_key=ce6162de-a18f-4f0a-a1d8-43900ef59215`;
+    const [videos, setVideos] = useState([]);
+    // const [selectedvideo, setSelectedVideo] = useState(null);
 
     let content = null
+
+
 
     useEffect(() => {
         axios.get(url)
             .then(response => {
                 setVideos(response.data)
             })
-    }, [url])
 
-    console.log(url)
+        // getVideoById(videoId) {
+        // axios.get(urls)
+        //     .then(response => {
+        //         setSelectedVideo(response.videos.id)
+        //     })
 
-    if (videos) {
+        // console.log(getVideoById)
+    }
+        // check url to display the correct video
+        // const currentVideoId = id || videos[0].id
+        // setSelectedVideo(currentVideoId)
+    }, [url]);
 
-        const handleClickVideo = (id) => {
-            videos.map(details => {
-                if (details.id === id) {
-                    setVideos(details)
-                }
-            })
-        }
 
-        content =
-            <>
-                {/* Header */}
-                <Header />
-                <Video poster={videos.image} />
-                <div className="belowvideo">
-                    <div className="belowvideo__sectioncomments">
-                        <Section sections={videos} />
-                        <Commentbox commentlength={videos.comments.length} commentword={"Comments"} />
-                        <Comment comments={videos.comments} />
-                    </div>
-                    <div className="belowvideo__nextvideo">
-                        <p className="belowvideo__nextvideo--text">next videos</p>
-                        <Nextvideo handleClickVideo={handleClickVideo} nextvideo={videos} activevideoid={videos.id} />
-                    </div>
-                </div>
-            </>
+// state = {
+//     videos: [],
+//     activeVideo: null,
+//   };
+//   componentDidMount() {
+//     axios
+//       .get(`${API_URL}videos/?api_key=${API_KEY}`)
+//       .then((response) => {
+//         this.setState({
+//           videos: response.data,
+//         });
+//         const activeVideoId = this.props.match.params.id || response.data[0].id;
+//         this.getActiveVideo(activeVideoId);
+//       })
+//       .catch((err) => console.log(err));
+//   }
+//   componentDidUpdate(prevProps) {
+//     const videoId = this.props.match.params.id;
+//     const prevVideoId = prevProps.match.params.id;
+//     if (videoId !== prevVideoId) {
+//       if (typeof videoId === "undefined") {
+//         const defaultVideoId = this.state.videos[0].id;
+//         this.getActiveVideo(defaultVideoId);
+//       }
+//       this.getActiveVideo(videoId).then(window.scrollTo(0, 0));
+//     }
+//   }
+//   getActiveVideo(videoId) {
+//     return axios
+//       .get(`${API_URL}videos/${videoId}?api_key=${API_KEY}`)
+//       .then((response) => {
+//         this.setState({
+//           activeVideo: response.data,
+//         });
+//       })
+//       .catch((err) => console.log(err));
+//   }
 
+
+if (videos) {
+
+    const handleClickVideo = (id) => {
+        videos.map(details => {
+            if (details.id === id) {
+                setVideos(details)
+            }
+        })
     }
 
-    return (
-        <div>{content}</div>
-    )
+    // code from Fahd
+    //Create a method to fetch matching id video
+    //   fetchVideo = (videoId) => {
+    //     axios
+    //       .get(`…/videos/${videoId}`)
+    //       .then((response) =>
+    //         this.setState({
+    //           selectedMovie: response.data,
+    //         })
+    //       )
+    //       .catch((err) => console.error(err));
+    //   };
+    // Add turnery && 
+    return
+    <>
+        {/* Header */}
+        < Header />
+        <Video poster={videos.image} />
+        <div className="belowvideo">
+            <div className="belowvideo__sectioncomments">
+                <Section sections={videos} />
+                <Commentbox commentlength={videos.comments.length} commentword={"Comments"} />
+                <Comment comments={videos.comments} />
+            </div>
+            <div className="belowvideo__nextvideo">
+                <p className="belowvideo__nextvideo--text">next videos</p>
+                <Nextvideo handleClickVideo={handleClickVideo} nextvideo={videos} activevideoid={videos.id} />
+            </div>
+        </div>
+    </>
+}
+
+return (
+    <div>{content}</div>
+)
 
 
     // Sprint 1 from Below
